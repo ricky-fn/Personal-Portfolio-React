0b854800c553097b8a6ad496a23c6df99afd9fc7
Ricky Jiang <mr.jiangxue@hotmail.com>
2023-03-10 16:18:02 +0800

    Commit from TeleportHQ

--- src/views/page.js
+++ src/views/page.js
@@ -190,6 +190,174 @@ const Page = (props) => {
             <IMac rootClassName="-mac-root-class-name"></IMac>
           </div>
         </section>
+        <div className="page-i-mac-section">
+          <div className="page-logos">
+            <img
+              alt="GoogleChromeLogowine18710"
+              src="/playground_assets/googlechromelogowine18710-sd19.svg"
+              className="page-chrome"
+            />
+            <img
+              alt="vue918260"
+              src="/playground_assets/vue918260-fg4b.svg"
+              className="page-vue"
+            />
+            <img
+              alt="Figma18240"
+              src="/playground_assets/figma18240-r4cz.svg"
+              className="page-figma"
+            />
+            <div className="page-nodejs">
+              <div className="page-group">
+                <img
+                  alt="Vector8266"
+                  src="/playground_assets/vector8266-1pb9.svg"
+                  className="page-vector"
+                />
+                <img
+                  alt="Vector8267"
+                  src="/playground_assets/vector8267-r418.svg"
+                  className="page-vector01"
+                />
+                <img
+                  alt="Vector8268"
+                  src="/playground_assets/vector8268-w9m.svg"
+                  className="page-vector02"
+                />
+                <div className="page-group1">
+                  <img
+                    alt="Vector8270"
+                    src="/playground_assets/vector8270-aeae.svg"
+                    className="page-vector03"
+                  />
+                  <div className="page-maskgroup">
+                    <div className="page-group2"></div>
+                    <div className="page-group3"></div>
+                  </div>
+                  <div className="page-maskgroup1">
+                    <div className="page-group4"></div>
+                    <div className="page-group5"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="page-cypress">
+              <div className="page-group6">
+                <img
+                  alt="Vector8250"
+                  src="/playground_assets/vector8250-8kpa.svg"
+                  className="page-vector04"
+                />
+                <img
+                  alt="Vector8251"
+                  src="/playground_assets/vector8251-7dg.svg"
+                  className="page-vector05"
+                />
+                <img
+                  alt="Vector8252"
+                  src="/playground_assets/vector8252-3s4v.svg"
+                  className="page-vector06"
+                />
+                <img
+                  alt="Vector8258"
+                  src="/playground_assets/vector8258-o10g.svg"
+                  className="page-vector07"
+                />
+                <img
+                  alt="Vector8259"
+                  src="/playground_assets/vector8259-hm5a.svg"
+                  className="page-vector08"
+                />
+              </div>
+            </div>
+            <div className="page-react">
+              <div className="page-group7">
+                <img
+                  alt="Vector8238"
+                  src="/playground_assets/vector8238-fump.svg"
+                  className="page-vector09"
+                />
+                <img
+                  alt="Vector8239"
+                  src="/playground_assets/vector8239-2b2.svg"
+                  className="page-vector10"
+                />
+              </div>
+            </div>
+            <img
+              alt="sass118246"
+              src="/playground_assets/sass118246-b5r.svg"
+              className="page-sass"
+            />
+          </div>
+          <img
+            alt="svgmanworkingonlaptopicon41828"
+            src="/playground_assets/svgmanworkingonlaptopicon41828-bm7k.svg"
+            className="page-manwithlaptop"
+          />
+        </div>
+        <div className="page-contact-section">
+          <img
+            src="/playground_assets/background9219-o1et-800h.png"
+            alt="background9219"
+            className="page-background"
+          />
+          <div className="page-container5">
+            <div className="page-photo">
+              <img
+                src="/playground_assets/dsc0644711075-8mhq-200w.png"
+                alt="DSC0644711075"
+                className="page-dsc064471"
+              />
+            </div>
+            <div className="page-content3">
+              <div className="page-container6">
+                <div className="page-stayconnected">
+                  <span className="page-title">
+                    <span>To stay connected:</span>
+                  </span>
+                  <span className="page-text47">
+                    <span>ricky.jc2022@gmail.com</span>
+                  </span>
+                  <span className="page-text49">
+                    <span>LinkedIn Profile</span>
+                  </span>
+                  <span className="page-text51">
+                    <span>View resume</span>
+                  </span>
+                </div>
+                <div className="page-thiswebsite">
+                  <span className="page-title1">
+                    <span>This website:</span>
+                  </span>
+                  <div className="page-row">
+                    <span className="page-text54">
+                      <span>is powered by:</span>
+                    </span>
+                    <img
+                      src="/playground_assets/reactjsar2119534-ubh.svg"
+                      alt="reactjsar2119534"
+                      className="page-reactjsar211"
+                    />
+                  </div>
+                  <div className="page-row1">
+                    <span className="page-text56">
+                      <span>is hosted on:</span>
+                    </span>
+                    <img
+                      src="/playground_assets/githublogo9530-j4sd.svg"
+                      alt="githublogo9530"
+                      className="page-githublogo"
+                    />
+                  </div>
+                </div>
+                <span className="page-text58">
+                  <span>Bye!</span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
